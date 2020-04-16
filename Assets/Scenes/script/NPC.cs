c2087c7127fe4906978e2f60fcc2e1c3755cdf48
Choco-T972 <choco_t@yeah.net>
2020-04-16 22:00:56 +0800

    Click E to chat
    Have no idea why clicking E might not work somtime.

--- Assets/Scenes/script/NPC.cs
+++ Assets/Scenes/script/NPC.cs
@@ -1,14 +1,68 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class NPC : MonoBehaviour
 {
+    // UI Prefabs
+    [SerializeField]
+    private Text textPrefab = null;
+
+    // Get Tip Text
+    private Text tip;
+
+    // Get Dialog container
+    private GameObject dialogCont;
+    private GameObject dialogAns;
+    private Text textAns;
+
+    private void Awake()
+    {
+        // Find tip
+        tip = GameObject.Find("UI_Tip").GetComponent<Text>();
+        // Find container
+        dialogCont = GameObject.Find("UI_Dialog_Cont");
+        dialogAns = GameObject.Find("UI_Dialog_Answer");
+        textAns = dialogAns.GetComponent<Text>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        DialogController._ins.ShowDialog(this.name, "Hello");
+             
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        tip.text = "E to chat";
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            Debug.Log("e");
+            //clear tip
+            tip.text = "";
+            // Display dialog container
+            dialogCont.SetActive(true);
+            dialogAns.GetComponent<Text>().text = "Hello";
+         
+
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         DialogController._ins.CloseDialog();
+    }
+
+    // Creates a textbox showing the the line of text
+    //void CreateContentView(string text)
+    //{
+    //    myText = Instantiate(textPrefab) as Text;
+    //    myText.text = text;
+    //    myText.transform.SetParent(dialogCont.transform, false);
+    //}
+
+    public void showDialogAnswer()
+    {
+        DialogController._ins.ShowDialog(this.name, textAns.text);
     }
 }
